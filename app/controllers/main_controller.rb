81bb90b25ecf5370ab992bc55d6800a55566af1d
Ruby-team <damonaify@gmail.com>
2017-05-09 20:19:48 +0200

    updated main page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,7 @@
 class MainController < ApplicationController
     before_action :authenticate_model!, except: [:index, :show]
   def index
+    @notes = Note.last(10).reverse
+    @activities = Activity.last(15).reverse
   end
 end
