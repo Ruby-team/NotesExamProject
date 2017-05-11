38d69fb5d3dfdba02029610ccf8f84efaa77ea5d
Ruby-team <damonaify@gmail.com>
2017-05-11 17:21:39 +0200

    fixed responsive web design

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,6 +5,17 @@ class NotesController < ApplicationController
     @activity = Activity.find(params[:activity_id])
     @note = Note.new
   end
+
+  def update
+    @activity = Activity.find(params[:activity_id])
+    @note = Note.find(params[:id])
+    if @note.update(note_params)
+      redirect_to @activity
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @activity = Activity.find(params[:activity_id])
     @note = @activity.notes.create(note_params)
